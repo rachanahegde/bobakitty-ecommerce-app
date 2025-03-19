23ee43cb60ee2372b60d703c62a94da4684b5011
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2025-03-19 21:30:09 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "./",
+  json: {
+    stringify: true, // Enables importing JSON files
+  },
 });
