f79c217117d990a4fe8e16ace689595601741bb9
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2025-03-29 12:40:12 +0000

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -60,19 +60,21 @@ const Home = () => {
 
   return (
     <div className="mt-6 text-center relative">
-      <h1 className="text-2xl font-fredoka font-bold text-light-pink">
+      <h1 className="text-md sm:text-2xl font-fredoka font-bold text-light-pink max-w-2xs mx-auto sm:max-w-3xl md:max-w-xl">
         Sip, Purr, Relax: The Ultimate Bubble Tea & Cat Café Experience!
       </h1>
+      {/* Animated paw prints */}
       <PawPrint />
+
       {/* Image Grid */}
-      <div className="mt-10 max-w-3xl p-6 mx-auto rounded-2xl bg-white/25 backdrop-blur-lg shadow-xl border border-white/20">
-        <div className="grid grid-cols-3 gap-6">
+      <div className="mt-10 max-w-xs md:max-w-2xl lg:max-w-3xl p-4 md:p-6 mx-auto rounded-2xl bg-white/25 backdrop-blur-lg shadow-xl border border-white/20">
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-6">
           <img
             src={person_drinking_boba_1}
             alt="Person drinking boba"
             className="rounded-lg shadow-md"
           />
-          <div className="grid gap-y-6">
+          <div className="grid gap-y-4 md:gap-y-6">
             <img
               src={cat_with_boba}
               alt="Cat 1"
@@ -87,12 +89,13 @@ const Home = () => {
           <img
             src={person_drinking_boba_2}
             alt="Person drinking boba"
-            className="rounded-lg shadow-md"
+            className="hidden md:flex rounded-lg shadow-md"
           />
         </div>
       </div>
+
       {/* Buttons */}
-      <div className="mt-8 flex flex-col md:flex-row justify-center gap-6">
+      <div className="mt-10 md:mt-8 flex flex-row justify-center gap-6">
         <Link
           to="/shop"
           className="px-6 py-3 bg-gradient-to-r from-light-pink to-medium-pink text-dark-purple font-montserrat font-bold text-sm rounded-full shadow-md hover:scale-105 transition-transform"
@@ -106,31 +109,36 @@ const Home = () => {
           Book a Visit
         </Link>
       </div>
+
       {/* Featured Drinks */}
-      <h1 className="text-2xl font-fredoka font-bold text-light-pink mt-8">
+      <h1 className="text-xl md:text-2xl font-fredoka font-bold text-light-pink mt-8">
         Featured Drinks
       </h1>
-      <div className="mt-6">
+
+      <div className="mt-8 md:mt-6">
         {/* Drink Cards with Left/Right Navigation */}
-        <div className="relative w-full max-w-3xl mx-auto flex items-center">
+        <div className="relative w-full max-w-[320px] md:max-w-2xl lg:max-w-3xl mx-auto flex items-center">
           {/* Left Arrow Button */}
-          <button onClick={handlePrev} className="absolute left-[-50px] w-10">
+          <button
+            onClick={handlePrev}
+            className="absolute left-[-38px] md:left-[-40px] lg:left-[-50px] w-8 lg:w-10"
+          >
             <img src={left_arrow} />
           </button>
 
           {/* Drinks Grid */}
-          <div className="grid grid-cols-3 gap-8 w-full">
+          <div className="grid grid-cols-3 gap-6 lg:gap-8 w-full">
             {drinks.slice(currentIndex, currentIndex + 3).map((drink) => (
               <div
                 key={drink.id}
-                className="flex flex-col items-center bg-white/30 backdrop-blur-lg shadow-lg pt-6 pr-2 pl-2 rounded-xl"
+                className="flex flex-col items-center bg-white/30 backdrop-blur-lg shadow-lg pt-4 md:pt-6 pr-2 pl-2 rounded-xl"
               >
                 <img
                   src={drink.image}
                   alt={drink.name}
-                  className="w-45 h-45 rounded-lg object-cover shadow-md"
+                  className="w-25 h-25 md:w-40 md:h-40 lg:w-45 lg:h-45 rounded-lg object-cover shadow-md"
                 />
-                <p className="mt-4 mb-2 text-sm font-montserrat font-semibold text-light-purple">
+                <p className="mt-4 mb-2 text-xs md:text-sm font-montserrat font-semibold text-light-purple">
                   {drink.name}
                 </p>
               </div>
@@ -138,11 +146,15 @@ const Home = () => {
           </div>
 
           {/* Right Arrow Button */}
-          <button onClick={handleNext} className="absolute right-[-50px] w-10">
+          <button
+            onClick={handleNext}
+            className="absolute right-[-38px] md:right-[-42px] lg:right-[-50px] w-8 lg:w-10"
+          >
             <img src={right_arrow} />
           </button>
         </div>
       </div>
+
       {/* View Menu Btn */}
       <div className="mt-10">
         <Link
